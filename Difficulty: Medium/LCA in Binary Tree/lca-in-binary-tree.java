87d7543da4235d947a5c982e6d9c2af3ece84b1a
Sameera eeda <148689070+Sameeraeeda@users.noreply.github.com>
2025-06-19 14:48:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.java
@@ -0,0 +1,30 @@
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+
+class Solution {
+    // Function to return the lowest common ancestor in a Binary Tree.
+    Node lca(Node root, int n1, int n2) {
+        // Your code here
+        if(root==null )return null;
+        if(root.data==n1 || root.data==n2){
+            return root;
+        }
+        Node lcaleft=lca(root.left,n1,n2);
+        Node lcaright=lca(root.right,n1,n2);
+        if(lcaleft!=null && lcaright!=null)return root;
+        if(lcaleft!=null && lcaright==null){
+            return lcaleft;
+        }
+        return lcaright;
+    }
+}
