eb6d96771e23988a30c0a96f46521b08646cc2ee
Sameera eeda <148689070+Sameeraeeda@users.noreply.github.com>
2025-08-10 19:50:34 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.66%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,25 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int l=0;
+        int low=0;
         int mid=0;
-        int r=nums.length-1;
-        while(mid<=r){
+        int high=nums.length-1;
+        while(mid<=high){
             if(nums[mid]==0){
-                swap(nums,l,mid);
-                l++;
+                swap(nums,low,mid);
+                low++;
                 mid++;
             }
             else if(nums[mid]==1){
                 mid++;
             }
-            else if(nums[mid]==2){
-                swap(nums,mid,r);
-                r--;
+            else{
+                swap(nums,mid,high);
+                high--;
             }
         }
         
     }
-    public void swap(int nums[],int i,int j){
+    private static void swap(int nums[],int i,int j){
         int temp=nums[i];
         nums[i]=nums[j];
         nums[j]=temp;
