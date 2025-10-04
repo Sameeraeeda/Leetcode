f56dcb234e372cfecfda4655ba5292a33540efd4
Sameera eeda <148689070+Sameeraeeda@users.noreply.github.com>
2025-10-04 10:59:39 +0530

    Time: 0 ms (100%), Space: 46 MB (49.63%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,50 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int idx1=leftpos(nums,target);
-        int idx2=rightpos(nums,target);
-        int res[]=new int[2];
-        res[0]=idx1;
-        res[1]=idx2;
-        return res;
+       int idx1=leftpos(nums,target);
+       int idx2=rightpos(nums,target);
+       if(idx1==-1 && idx2==-1){
+        return new int[]{-1,-1};
+       }
+       return new int[]{idx1,idx2};
     }
     public int leftpos(int nums[],int target){
-        int l=0;
-        int r=nums.length-1;
-        int res=-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target){
-                res=mid;
-                r=mid-1;
-            }
-            else if(nums[mid]>target){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
+       int l=0;
+       int r=nums.length-1;
+       int index=-1;
+       while(l<=r){
+        int mid=l+(r-l)/2;
+        
+        if(nums[mid]==target){
+            index=mid;
+            r=mid-1;
         }
-        return res;
+        else if(nums[mid]>target){
+            r=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+       }
+       return index;
     }
     public int rightpos(int nums[],int target){
-        int l=0;
-        int r=nums.length-1;
-        int res=-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target){
-                res=mid;
-                l=mid+1;
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
-            }
+       int l=0;
+       int r=nums.length-1;
+       int index=-1;
+       while(l<=r){
+        int mid=l+(r-l)/2;
+        if(nums[mid]==target){
+            index=mid;
+            l=mid+1;
         }
-        return res;
+        else if(nums[mid]>target){
+            r=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+       }
+       return index;
     }
    
 }
