d09d5a31c57f11dc835de68fb8d2c06836ba1c3c
Sameera eeda <148689070+Sameeraeeda@users.noreply.github.com>
2025-10-08 12:34:59 +0530

    Time: 16 ms (6.92%), Space: 58.2 MB (82.28%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -12,35 +12,19 @@ class Solution {
     public boolean isPalindrome(ListNode head) {
         Stack<Integer> st=new Stack<>();
         ListNode temp=head;
-       while(temp!=null){
-        st.push(temp.val);
-        temp=temp.next;
-       }
-       ListNode dummy=null;
-       ListNode tail=null;
-       while(!st.isEmpty()){
-        ListNode newNode=new ListNode(st.peek());
-        if(dummy==null){
-              dummy=newNode;
-              tail=newNode;
+        while(temp!=null){
+            st.push(temp.val);
+            temp=temp.next;
         }
-        else{
-            tail.next=newNode;
-            tail=newNode;
-        }
-        st.pop();
-       }
-       return isPalindromeres(dummy,head);
-    }
-    public static boolean isPalindromeres(ListNode dummy,ListNode head){
-        ListNode temp1=dummy;
-        ListNode temp2=head;
-        while(temp1!=null && temp2!=null){
-            if(temp1.val!=temp2.val){
+        temp=head;
+        while(!st.isEmpty() && temp!=null ){
+            int num1=temp.val;
+            int num2=st.pop();
+            if(num1!=num2){
                 return false;
             }
-            temp1=temp1.next;
-            temp2=temp2.next;
+            temp=temp.next;
+
         }
         return true;
     }
