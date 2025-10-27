596ea4464283542fe6116504f668c25cbbb927c2
Sameera eeda <148689070+Sameeraeeda@users.noreply.github.com>
2025-10-27 10:35:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.java
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -2,19 +2,19 @@ class Solution {
     // Function to find equilibrium point in the array.
     public static int findEquilibrium(int arr[]) {
         // code here
-        int total=0;
-        for(int a:arr){
-            total+=a;
-        }
         int left=0;
-        int rightsum=0;
-        for(int i=0;i<arr.length;i++){
-            left+=arr[i];
-            if(left==total){
-                return i;
-            }
-            total=total-arr[i];
-        }
-        return -1;
+         int sum=0;
+         for(int a:arr){
+             sum+=a;
+         }
+         for(int i=0;i<arr.length;i++){
+             
+             int right=sum-left-arr[i];
+             if(left==right){
+                 return i;
+             }
+             left+=arr[i];
+         }
+         return -1;
     }
 }
